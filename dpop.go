b4987a3179f812213b759cfde372dcf70ae65456
sandeep <smukk9@gmail.com>
2026-02-01 22:01:51 -0800

    add thubprint part

--- dpop.go
+++ dpop.go
@@ -1,26 +1,61 @@
 package main
 
 import (
+	"crypto"
+	"encoding/base64"
+	"errors"
 	"fmt"
 	"net/http"
 
-	"github.com/square/go-jose/jwt"
+	"gopkg.in/square/go-jose.v2/jwt"
 )
 
 //verify dpop header
 
-func HandelDpopTokenRequest(r *http.Request) {
+func HandelDpopTokenRequest(r *http.Request) (string, error) {
 
 	dpopHearder := r.Header.Get("dpop")
 
-	fmt.Println("Recevied a dpop header: " + dpopHearder)
+	// fmt.Println("Recevied a dpop header: " + dpopHearder)
 	token, err := jwt.ParseSigned(dpopHearder)
 
 	if err != nil {
-		fmt.Println("Invalid DPoP JWT format:", err)
-		return
+		return "", errors.New("Invalid DPOP JWT format")
 	}
 
-	fmt.Printf("DPoP Headers: %+v\n", token.Headers)
+	var claims map[string]interface{}
+
+	//TODO: verify claims match the request
+	err = token.UnsafeClaimsWithoutVerification(&claims)
+	if err != nil {
+		return "", errors.New("Unable to calculate thumbrpint, invaild JsonWebKey")
+	}
+
+	// //figure out what token has
+	// for _, v := range token.Headers {
+	// 	fmt.Print("----------")
+	// 	fmt.Print(v)
+	// 	fmt.Print("----------")
+	// }
+
+	//get the header[0] that has the JsonWebKey
+	//must contiain only one. Pick the first one.
+	jk := token.Headers[0].JSONWebKey
+
+	jkt, err := jk.Thumbprint(crypto.SHA256)
+	if err != nil {
+		return "", errors.New("Unable to calculate thumbrpint, invaild JsonWebKey")
+	}
+	//https://datatracker.ietf.org/doc/html/rfc9449#name-jwk-thumbprint-confirmation
+	//JWK SHA-256 Thumbprint confirmation method. The value of the jkt member MUST be the base64url encoding (as defined in [RFC7515]) of the JWK SHA-256 Thumbprint (according to [RFC7638]) of the DPoP public key (in JWK format) to which the access token is bound.
+	fmt.Println("----------")
+	fmt.Println(base64.RawURLEncoding.EncodeToString(jkt))
+	fmt.Println("----------")
+
+	fmt.Println("Subject:", claims["sub"])
+	fmt.Println("HTM:", claims["htm"])
+	fmt.Println("HTU:", claims["htu"])
+
+	return base64.RawURLEncoding.EncodeToString(jkt), nil
 
 }
--- token.go
+++ token.go
@@ -3,28 +3,49 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log/slog"
 	"net/http"
+	"os"
+	"time"
 )
 
-type TokenDB struct {
+// Initialize logger at package level
+var logger = slog.New(slog.NewJSONHandler(os.Stdout, nil))
+
+type TokenRecord struct {
 	AccessToken     string
 	Thumbprint_DPOP string
-	ExpireAt        string
-	CreatedAt       string
+	ExpireAt        int64
+	CreatedAt       int64
+	Client_id       string
+	GrantType       string
+	TokenType       string
 }
+
+var tokenStore []TokenRecord
 
 type TokenResponse struct {
 	AccessToken string `json:"access_token"`
-	Expiry      string `json:"expiry"`
+	Expiry      int64  `json:"expiry"`
 	TokenType   string `json:"token_type"`
 }
 
 func tokenEndpoint(w http.ResponseWriter, r *http.Request) {
 
 	r.ParseForm()
+	if err := r.ParseForm(); err != nil {
+		http.Error(w, "Failed to parse form", http.StatusBadRequest)
+		return
+	}
 
+	//TODO: 1. move this to extracDPOPTHumprint emthods
+	var jkt string
 	if r.Header.Get("dpop") != "" {
-		HandelDpopTokenRequest(r)
+		var err error
+		jkt, err = HandelDpopTokenRequest(r)
+		if err != nil {
+			http.Error(w, "dpop error", http.StatusBadRequest)
+		}
 	}
 	clientId := r.FormValue("client_id")
 	clientSecret := r.FormValue("client_secret")
@@ -57,11 +78,52 @@ func tokenEndpoint(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	var res TokenResponse
-	res.AccessToken = access_token
-	res.TokenType = "Bearer"
-	res.Expiry = "299"
+	var ts TokenRecord
+	ts.AccessToken = access_token
+	ts.CreatedAt = time.Now().Unix()
+	ts.ExpireAt = time.Now().Unix() + 300
 
+	if r.Header.Get("dpop") != "" && jkt != "" {
+		ts.Thumbprint_DPOP = jkt
+		println(jkt)
+		ts.TokenType = "DPOP"
+	} else {
+		ts.Thumbprint_DPOP = ""
+		ts.TokenType = "Bearer"
+	}
+
+	ts.Client_id = clientId
+	ts.GrantType = grantType
+	tokenStore = append(tokenStore, ts)
+	fmt.Println("--------------------------")
+	printTokenStore(tokenStore)
+	fmt.Println("--------------------------")
+
+	var res TokenResponse
+	res.AccessToken = ts.AccessToken
+	res.TokenType = ts.TokenType
+	res.Expiry = ts.ExpireAt
+
+	logger.Info("token store:", "count", len(tokenStore), "tokens", tokenStore)
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(res)
 }
+
+// for debuging only
+// TODO:L: remove
+func printTokenStore(tokens []TokenRecord) {
+	for i, t := range tokens {
+		fmt.Printf("Record %d:\n", i+1)
+		fmt.Printf("  AccessToken: %s\n", t.AccessToken)
+		fmt.Printf("  Thumbprint_DPOP: %s\n", t.Thumbprint_DPOP)
+		fmt.Printf("  ExpireAt: %d\n", t.ExpireAt)
+		fmt.Printf("  CreatedAt: %d\n", t.CreatedAt)
+		fmt.Println()
+	}
+}
+
+// TODO: clean up from above and put it here
+// func generateAccessToken()
+
+// TODO; createTokenRecord
+// func createTokenRecord(accessToken, clientID, grantType, jkt string) TokenRecord {}
